4a2cd0ca62032eb1d13b154d8eb03dcde376a83e
unknown <jameskagunga2022@gmail.com>
2023-09-19 03:27:18 -0700

    added changes

--- client-side/src/requestMethods.js
+++ client-side/src/requestMethods.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const BASE_URL="http://143.110.150.132:5000/api/";
-const BASE_URL_1="http://localhost:8000/api/";
+const BASE_URL_1="https://api.duboisbeauty.co.ke/api/";
 
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMGI2NzM2ZjJmZDM2YTBlMjA5YWE3MSIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2NDQ5MTQ1MDUsImV4cCI6MTY0NTE3MzcwNX0.TRfdqYrZXhFrj7kYmpv1jnQtYMxMPu-unP-g9cYux4Q";
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMGI2NzM2ZjJmZDM2YTBlMjA5YWE3MSIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2NDQ5MTQ1MDUsImV4cCI6MTY0NTE3MzcwNX0.TRfdqYrZXhFrj7kYmpv1jnQtYMxMPu-unP-g9cYux4Q";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
